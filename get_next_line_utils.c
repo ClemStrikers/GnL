d56cf0e57192ea81fb071c7cfcb19d04b43368e1
Clement Paolin <clempaol@c1r4p9.42nice.fr>
2025-11-18 13:25:00 +0100

    2

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,11 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clempaol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: clempaol <clempaol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 13:40:04 by clempaol          #+#    #+#             */
-/*   Updated: 2025/11/17 13:40:06 by clempaol         ###   ########.fr       */
+/*   Updated: 2025/11/18 13:23:08 by clempaol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (&s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return (&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char s1, char s2)
+{
+	char	*n;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+	{
+		s1 = malloc(1);
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	n = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!n)
+		return (NULL);
+	i = -1;
+	while (s1[i++])
+		n[i++] = s2[j++];
+	n[i] = '\0';
+	free(s1);
+	return (n);
+}
